ee39f7dd6395a1040e94c89692e90eac74050c04
tawhid12 <tawhid99@gmail.com>
2023-07-12 17:51:25 +0600

    correction

--- app/Console/Commands/DatabaseBackUp.php
+++ app/Console/Commands/DatabaseBackUp.php
@@ -1,10 +1,11 @@
 <?php
-  
+
 namespace App\Console\Commands;
-  
+
 use Illuminate\Console\Command;
 use Carbon\Carbon;
-   
+use ZipArchive;
+
 class DatabaseBackUp extends Command
 {
     /**
@@ -13,14 +14,14 @@ class DatabaseBackUp extends Command
      * @var string
      */
     protected $signature = 'database:backup';
-  
+
     /**
      * The console command description.
      *
      * @var string
      */
-    protected $description = 'Command description';
-  
+    protected $description = 'Create a database backup and upload as a ZIP archive';
+
     /**
      * Create a new command instance.
      *
@@ -30,7 +31,7 @@ class DatabaseBackUp extends Command
     {
         parent::__construct();
     }
-  
+
     /**
      * Execute the console command.
      *
@@ -38,13 +39,36 @@ class DatabaseBackUp extends Command
      */
     public function handle()
     {
+        $backupDirectory = storage_path('app/backup');
         $filename = "backup-" . Carbon::now()->format('Y-m-d') . ".sql";
-  
-        $command = "mysqldump --user=" . env('DB_USERNAME') ." --password=" . env('DB_PASSWORD') . " --host=" . env('DB_HOST') . " " . env('DB_DATABASE') . "  | sql > " . storage_path() . "/app/backup/" . $filename;
-  
-        $returnVar = NULL;
-        $output  = NULL;
-  
-        exec($command, $output, $returnVar);
+        $zipFilename = "backup-" . Carbon::now()->format('Y-m-d') . ".zip";
+
+        $command = sprintf(
+            'mysqldump --user=%s --password=%s --host=%s %s > %s',
+            env('DB_USERNAME'),
+            env('DB_PASSWORD'),
+            env('DB_HOST'),
+            env('DB_DATABASE'),
+            $backupDirectory . '/' . $filename
+        );
+
+        exec($command);
+
+        $zip = new ZipArchive();
+        $zipPath = $backupDirectory . '/' . $zipFilename;
+
+        if ($zip->open($zipPath, ZipArchive::CREATE | ZipArchive::OVERWRITE) === true) {
+            $zip->addFile($backupDirectory . '/' . $filename, $filename);
+            $zip->close();
+
+            // Code to upload the ZIP file goes here
+
+            // Delete the temporary SQL file
+            unlink($backupDirectory . '/' . $filename);
+
+            $this->info('Database backup created and uploaded successfully.');
+        } else {
+            $this->error('Failed to create ZIP archive.');
+        }
     }
 }
