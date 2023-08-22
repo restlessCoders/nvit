944a3a48bb05eae74cb5307c7538e202951feec2
tawhid12 <tawhid99@gmail.com>
2023-08-22 16:44:35 +0600

    correction

--- resources/views/report/accounts/daily_collection_by_mr.blade.php
+++ resources/views/report/accounts/daily_collection_by_mr.blade.php
@@ -218,8 +218,8 @@
 							@if($p->bid !=0)
 							{{\DB::table('student_batches')->where('student_id',$p->studentId)->where('batch_id',$p->bid)->first()->course_price}}{{--$p->cPayable--}}
 							@else
-							{{--$p->course_id--}}
-							{{\DB::table('student_batches')->where('student_id',$p->studentId)->where('course_id',$p->course_id)->first()->course_price}}{{--$p->cPayable--}}
+							{{$p->course_id}}
+							{{--\DB::table('student_batches')->where('student_id',$p->studentId)->where('course_id',$p->course_id)->first()->course_price--}}{{--$p->cPayable--}}
 							@endif
 
 						</td>
