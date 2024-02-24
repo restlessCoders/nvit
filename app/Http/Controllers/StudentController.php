622b942c51ecd1bdb8a8debd739a7b3ef9a17106
tawhid12 <tawhid99@gmail.com>
2024-02-24 16:16:08 +0600

    correction

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -755,18 +755,15 @@ class StudentController extends Controller
        
 
         $e_data = DB::table('student_batches')
-        ->selectRaw("student_batches.batch_id, batches.batchId")
-        ->leftJoin('batches', 'batches.id', '=', 'student_batches.batch_id')
-        ->where('student_batches.student_id', $request->id)
-        ->where('student_batches.status', 2)
-        ->where('student_batches.is_drop', 1)
-        ->whereNotIn('student_batches.batch_id', $curbatchId)
-        ->groupBy('student_batches.batch_id', 'batches.batchId')
-        ->get();
-    
+            ->selectRaw("student_batches.batch_id,batches.batchId")
+            ->join('batches', 'batches.id', '=', 'student_batches.batch_id', 'left')
+            ->where(['student_id' => $request->id, 'student_batches.status' => 2, 'is_drop' => 1])
+            ->whereNotIn('student_batches.batch_id', $curbatchId)
+            ->groupBy('student_batches.batch_id', 'batches.batchId')
+            ->get();
             $queries = \DB::getQueryLog();
 
-            dd($queries);
+            //dd($queries);
         $data = '<label for="curbatchId" class="col-sm-3 col-form-label">From Batch</label>
             <div class="col-sm-9">
             <select class="js-example-basic-single form-control" id="curbatchId" name="curbatchId" required>
