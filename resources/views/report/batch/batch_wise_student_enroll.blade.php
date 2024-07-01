bbcff94bea62dfd0080de09db23d2e7a88b7ec21
tawhid12 <tawhid99@gmail.com>
2024-07-01 19:37:25 +0600

    correction

--- resources/views/report/batch/batch_wise_student_enroll.blade.php
+++ resources/views/report/batch/batch_wise_student_enroll.blade.php
@@ -591,18 +591,30 @@
 			});
 	});
 
-	$("input[name='date_range']").daterangepicker({
-			singleDatePicker: false,
-			//startDate: new Date(),
-			showDropdowns: true,
-			autoUpdateInput: true,
-			format: 'dd/mm/yyyy',
-		}).on('changeDate', function(e) {
-			var date = moment(e.date).format('YYYY/MM/DD');
-			$(this).val(date);
-		});
-		// Set the input value to an empty string after initialization
-		$("input[name='date_range']").val('');
+	$(document).ready(function() {
+
+var start = moment().subtract(29, 'days');
+var end = moment();
+
+function cb(start, end) {
+$('#date_range span').html(start.format('MMMM D, YYYY') + ' - ' + end.format('MMMM D, YYYY'));
+}
+
+$('#date_range').daterangepicker({
+startDate: start,
+endDate: end,
+ranges: {
+   'Today': [moment(), moment()],
+   'Yesterday': [moment().subtract(1, 'days'), moment().subtract(1, 'days')],
+   'Last 7 Days': [moment().subtract(6, 'days'), moment()],
+   'Last 30 Days': [moment().subtract(29, 'days'), moment()],
+   'This Month': [moment().startOf('month'), moment().endOf('month')],
+   'Last Month': [moment().subtract(1, 'month').startOf('month'), moment().subtract(1, 'month').endOf('month')]
+}
+}, cb);
+
+cb(start, end);
+	});
 </script>
 @if(Session::has('response'))
 <script>
