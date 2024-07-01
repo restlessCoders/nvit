7ad57d701992fbb1ec92774583a07752b8e2819e
tawhid12 <tawhid99@gmail.com>
2024-07-01 20:02:41 +0600

    correction

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -87,6 +87,8 @@ class ReportController extends Controller
                                 ->whereRaw('pd.studentId = paymentdetails.studentId')
                                 ->whereRaw('pd.batchId = paymentdetails.batchId');
                         });
+                        // Add check for deleted_at being NULL
+                        $query->whereNull('paymentdetails.deleted_at');
                         if (isset($request->date_range)) {
                             $date_range = explode('-', $request->date_range);
                             $from = \Carbon\Carbon::createFromTimestamp(strtotime($date_range[0]))->format('Y-m-d');
