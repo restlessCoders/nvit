25fc5538f2e3b16b90a28b4a4d356c4766cf0ea5
tawhid12 <tawhid99@gmail.com>
2024-07-02 17:27:23 +0600

    payment transfer delete complete

--- app/Http/Controllers/PaymentTransferController.php
+++ app/Http/Controllers/PaymentTransferController.php
@@ -49,7 +49,7 @@ class PaymentTransferController extends Controller
      */
     public function store(Request $request)
     {
-        echo $request->postingDate;
+        echo Carbon::createFromFormat('m/d/Y', $request->postingDate)->format('Y-m-d');
         dd($request);
         try {
             $to_exe_id = User::where('id', $request->to_exe_id)->first();
