236019263639ef03451297e5e2d43e0344cdf991
tawhid12 <tawhid99@gmail.com>
2024-07-11 15:56:05 +0600

    date range registration student report

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -226,7 +226,7 @@ class ReportController extends Controller
         }
 
         if ($request->batch_id) {
-            $allBatches->where('sb.batch_id', $request->batch_id);
+            $allBatches->where('student_batches.batch_id', $request->batch_id);
         }
         if ($request->refId) {
             $allBatches->where('students.refId', $request->refId);
@@ -235,16 +235,16 @@ class ReportController extends Controller
             $allBatches->where('students.executiveId', $request->executiveId);
         }
         if (strtolower(currentUser()) == 'accountmanager' || strtolower(currentUser()) == 'frontdesk') {
-            $allBatches->where('sb.status', 2);
+            $allBatches->where('student_batches.status', 2);
         }
         if (strtolower(currentUser()) == 'accountmanager') {
-            $allBatches->where('sb.isBundel', 0);
+            $allBatches->where('student_batches.isBundel', 0);
         }
         if ($request->status) {
-            $allBatches->where('sb.status', $request->status);
+            $allBatches->where('student_batches.status', $request->status);
         }
         if ($request->drop) {
-            $allBatches->where('sb.is_drop', 1);
+            $allBatches->where('student_batches.is_drop', 1);
         } else {
             $allBatches->where('sb.is_drop', 0);
         }
