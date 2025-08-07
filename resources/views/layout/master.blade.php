b37b3df2fb427423dd54e5fc76afaad5f5e7d59d
tawhid12 <tawhid99@gmail.com>
2025-08-07 15:50:43 +0600

    correction in route

--- resources/views/layout/master.blade.php
+++ resources/views/layout/master.blade.php
@@ -366,8 +366,8 @@
                                     <li class="has-submenu @if(currentUser() == 'superadmin' || currentUser() == 'operationmanager') @else d-none @endif">
                                         <a href="#">Management<div class="arrow-down"></div></a>
                                         <ul class="submenu">
-                                            <li><a href="@if(currentUser() == 'superadmin' || currentUser() == 'operationmanager') {{route(currentUser().'.dueReport')}} @endif">Due Report</a></li>
-                                            <li><a href="@if(currentUser() == 'superadmin' || currentUser() == 'operationmanager') {{route(currentUser().'.regReport')}} @endif">Reg Report (Date Range)</a></li>
+                                            <li><a href="@if(currentUser() == 'superadmin' || currentUser() == 'operationmanager' || currentUser() == 'accountmanager') {{route(currentUser().'.dueReport')}} @endif">Due Report</a></li>
+                                            <li><a href="@if(currentUser() == 'superadmin' || currentUser() == 'operationmanager' || currentUser() == 'accountmanager') {{route(currentUser().'.regReport')}} @endif">Reg Report (Date Range)</a></li>
                                         </ul>
                                     </li>
                                 </ul>
